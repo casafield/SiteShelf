f3d97d96c7c08bbb56a4da66fad6faab96416ae0
Casa <44913286+casadevelop@users.noreply.github.com>
2023-01-21 01:45:08 +0900

    feat(home): 詳細画面のパスを削除

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './screens/Home';
-import Detail from './screens/Detail';
-
-type RootStackParamList = {};
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +10,6 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Detail" component={Detail} />
       </Stack.Navigator>
     </NavigationContainer>
   );
